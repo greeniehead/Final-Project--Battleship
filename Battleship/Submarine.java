e620ed9453fcf8993789376b95c1ff92937b6091
greeniehead <GabyD3080@sduhsd.lan>
2017-05-23 11:02:56 -0700

    5-23-17 11:03 Dane
    Limit by board

--- Battleship/Submarine.java
+++ Battleship/Submarine.java
@@ -34,6 +34,48 @@ public class Submarine extends Actor
         {
             this.setLocation(this.getX(), this.getY() + 1);
         }
+        
+        if(!(Greenfoot.mouseDragged(this)))
+        {
+            if(this.getRotation() == 0)
+            {
+                while(this.getX() < 65)
+                {
+                    this.setLocation(this.getX()+1, this.getY());
+                }
+                while(this.getX() > 290)
+                {
+                    this.setLocation(this.getX()-1, this.getY());
+                }
+                while(this.getY() > 644)
+                {
+                    this.setLocation(this.getX(), this.getY()-1);
+                }
+                while(this.getY() < 360)
+                {
+                    this.setLocation(this.getX(), this.getY()+1);
+                }
+            }
+            else
+            {
+                while(this.getX() < 35)
+                {
+                    this.setLocation(this.getX()+1, this.getY());
+                }
+                while(this.getX() > 320)
+                {
+                    this.setLocation(this.getX()-1, this.getY());
+                }
+                while(this.getY() > 615)
+                {
+                    this.setLocation(this.getX(), this.getY()-1);
+                }
+                while(this.getY() < 400)
+                {
+                    this.setLocation(this.getX(), this.getY()+1);
+                }
+            }
+        }
     }
     
     public void horizontal()
