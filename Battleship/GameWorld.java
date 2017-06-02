7f3805d63c7679734d11f8ce7d1a8af958299e24
greeniehead <GabyD3080@sduhsd.lan>
2017-06-02 10:49:47 -0700

    6-2-17 10:50 Dane
    kjdsafbvgsa

--- Battleship/GameWorld.java
+++ Battleship/GameWorld.java
@@ -1,12 +1,6 @@
 import greenfoot.*;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 import java.util.*;
-=======
 import java.util.List;
->>>>>>> origin/master
->>>>>>> origin/master
 
 public class GameWorld extends World
 {
@@ -92,89 +86,189 @@ public class GameWorld extends World
         
         
         String[][] AIBoardArray = new String[10][10];
-        for(int x = 0; x < 10; x++){                
-                for(int y = 0; y < 10; y++){
-                    AIBoardArray[x][y] = "Empty";
-                }
-                    }
-                   
-        AICruiser AIcrui = new AICruiser();   
-        addObject(AIcrui, AICruiser.placeCruiser()[0]*60, AICruiser.placeCruiser()[1]*30);
-        if(AICruiser.placeCruiser()[3] > 5) AIcrui.setRotation(90);
-        
-        AISub AIsub = new AISub();   
-        addObject(AIsub, AISub.placeSub()[0]*60, AISub.placeSub()[1]*30);
-        if(AISub.placeSub()[3] > 5) AIsub.setRotation(90);
-       
-        
-        AIPatrol AIpat = new AIPatrol();
-        addObject(AIpat, AIPatrol.placePatrol()[0]*60, AIPatrol.placePatrol()[1]*30);
-        if(AIPatrol.placePatrol()[3] > 5) AIpat.setRotation(90);
-<<<<<<< HEAD
-        
-=======
-<<<<<<< HEAD
-        
-        
-=======
-             
->>>>>>> origin/master
->>>>>>> origin/master
-        AIBattle AIbat = new AIBattle();
-        addObject(AIbat, AIBattle.placeBattle()[0]*60, AIBattle.placeBattle()[1]*30);
-        if(AIBattle.placeBattle()[3] > 5) AIbat.setRotation(90);
-<<<<<<< HEAD
-        
-=======
-<<<<<<< HEAD
-        /*
-        System.out.println(getObjectsAt(80, 25, null).size());
-        System.out.println(getObjectsAt(95, 25, null).size());
-        AIBattle AIbat = new AIBattle();
-        addObject(AIbat, 95, 25);
-        //if(AIBattle.placeBattle()[3] > 5) AIbat.setRotation(90);
-        System.out.println(getObjectsAt(80, 25, null).size());
-        System.out.println(getObjectsAt(80, 25, null).get(0).toString());
-        System.out.println(getObjectsAt(95, 25, null).size());
-        System.out.println(getObjectsAt(95, 25, null).get(0).toString());
-        */
-=======
-                
->>>>>>> origin/master
->>>>>>> origin/master
-        AICarrier AIcarr = new AICarrier();
-        addObject(AIcarr, AICarrier.placeCarrier()[0]*60, AICarrier.placeCarrier()[1]*30);
-        if(AICarrier.placeCarrier()[3] > 5) AIcarr.setRotation(90);
-        
-        int count = 0;
-        int arrLength;
-        for(int x = 50; x < 350; x += 30)
-        {
-            for(int y = 25; y < 325; y += 30)
+        for(int x = 0; x < 10; x++)
+        {                
+            for(int y = 0; y < 10; y++)
             {
-                
-                arrLength = getObjectsAt(x, y, null).size();
-                Hit hitt = new Hit();
-                //System.out.println(arrLength);
-                /*
-                for(int i = 0; i < arrLength; i++)
-                {
-                    System.out.println(getObjectsAt(x, y, null).get(i).toString());
-                }
-                */
-                if((arrLength != 1)&&(arrLength != 0))
-                {
-                    addObject(hitt, x, y);
-                    count++;
-                }
-                
+                    AIBoardArray[x][y] = "Empty";
             }
         }
-        System.out.println(count);
+        
+        boolean dontPlaceOnTop = true;
+        while(dontPlaceOnTop)//should run and replace the ships until no ship is on one another
+        {
+            AICruiser AIcrui = new AICruiser();   
+            addObject(AIcrui, AICruiser.placeCruiser()[0]*30, AICruiser.placeCruiser()[1]*30);
+            if(AICruiser.placeCruiser()[3] > 5) AIcrui.setRotation(90);
+            
+            if(AIcrui.getRotation() == 0)
+            {
+                Hit hitt = new Hit();
+                addObject(hitt, AIcrui.getX(), AIcrui.getY());
+                Hit hittt = new Hit();
+                addObject(hittt, AIcrui.getX() -30, AIcrui.getY());
+                Hit hitttt = new Hit();
+                addObject(hitttt, AIcrui.getX() +30, AIcrui.getY());
+            }
+            else
+            {
+                Hit hitt = new Hit();
+                addObject(hitt, AIcrui.getX(), AIcrui.getY());
+                Hit hittt = new Hit();
+                addObject(hittt, AIcrui.getX(), AIcrui.getY() -30);
+                Hit hitttt = new Hit();
+                addObject(hitttt, AIcrui.getX(), AIcrui.getY() +30);
+            }
+            
+            
+            AISub AIsub = new AISub();   
+            addObject(AIsub, AISub.placeSub()[0]*30, AISub.placeSub()[1]*30);
+            if(AISub.placeSub()[3] > 5) AIsub.setRotation(90);
+            
+            if(AIsub.getRotation() == 0)
+            {
+                Hit hitt = new Hit();
+                addObject(hitt, AIsub.getX(), AIsub.getY());
+                Hit hittt = new Hit();
+                addObject(hittt, AIsub.getX() -30, AIsub.getY());
+                Hit hitttt = new Hit();
+                addObject(hitttt, AIsub.getX() +30, AIsub.getY());
+            }
+            else
+            {
+                Hit hitt = new Hit();
+                addObject(hitt, AIsub.getX(), AIsub.getY());
+                Hit hittt = new Hit();
+                addObject(hittt, AIsub.getX(), AIsub.getY() -30);
+                Hit hitttt = new Hit();
+                addObject(hitttt, AIsub.getX(), AIsub.getY() +30);
+            }
+            
+            
+            
+            AIPatrol AIpat = new AIPatrol();
+            addObject(AIpat, AIPatrol.placePatrol()[0]*30, AIPatrol.placePatrol()[1]*30);
+            if(AIPatrol.placePatrol()[3] > 5) AIpat.setRotation(90);
+            
+            if(AIpat.getRotation() == 0)
+            {
+                Hit hittt = new Hit();
+                addObject(hittt, AIpat.getX() -15, AIpat.getY());
+                Hit hitttt = new Hit();
+                addObject(hitttt, AIpat.getX() +15, AIpat.getY());
+            }
+            else
+            {
+                Hit hitt = new Hit();
+                addObject(hitt, AIpat.getX(), AIpat.getY());
+                Hit hittt = new Hit();
+                addObject(hittt, AIpat.getX(), AIpat.getY() -15);
+                Hit hitttt = new Hit();
+                addObject(hitttt, AIpat.getX(), AIpat.getY() +15);
+            }
+            
+            AIBattle AIbat = new AIBattle();
+            addObject(AIbat, AIBattle.placeBattle()[0]*30, AIBattle.placeBattle()[1]*30);
+            if(AIBattle.placeBattle()[3] > 5) AIbat.setRotation(90);
+            
+            if(AIbat.getRotation() == 0)
+            {
+                Hit hitt = new Hit();
+                addObject(hitt, AIbat.getX() +15, AIbat.getY());
+                Hit hittt = new Hit();
+                addObject(hittt, AIbat.getX() -15, AIbat.getY());
+                Hit hitttt = new Hit();
+                addObject(hitttt, AIbat.getX() +45, AIbat.getY());
+                Hit hittttt = new Hit();
+                addObject(hittttt, AIbat.getX() -45, AIbat.getY());
+            }
+            else
+            {
+                Hit hitt = new Hit();
+                addObject(hitt, AIbat.getX() +15, AIbat.getY() +15);
+                Hit hittt = new Hit();
+                addObject(hittt, AIbat.getX() -15, AIbat.getY() -15);
+                Hit hitttt = new Hit();
+                addObject(hitttt, AIbat.getX() +45, AIbat.getY() +45);
+                Hit hittttt = new Hit();
+                addObject(hitttt, AIbat.getX(), AIbat.getY() -45);
+            }
+            
+            /*
+            System.out.println(getObjectsAt(80, 25, null).size());
+            System.out.println(getObjectsAt(95, 25, null).size());
+            AIBattle AIbat = new AIBattle();
+            addObject(AIbat, 95, 25);
+            //if(AIBattle.placeBattle()[3] > 5) AIbat.setRotation(90);
+            System.out.println(getObjectsAt(80, 25, null).size());
+            System.out.println(getObjectsAt(80, 25, null).get(0).toString());
+            System.out.println(getObjectsAt(95, 25, null).size());
+            System.out.println(getObjectsAt(95, 25, null).get(0).toString());
+            */
+            
+            AICarrier AIcarr = new AICarrier();
+            addObject(AIcarr, AICarrier.placeCarrier()[0]*30, AICarrier.placeCarrier()[1]*30);
+            if(AICarrier.placeCarrier()[3] > 5) AIcarr.setRotation(90);
+            
+            if(AIcarr.getRotation() == 0)
+            {
+                Hit hitt = new Hit();
+                addObject(hitt, AIcarr.getX(), AIcarr.getY());
+                Hit hittt = new Hit();
+                addObject(hittt, AIcarr.getX() -30, AIcarr.getY());
+                Hit hitttt = new Hit();
+                addObject(hitttt, AIcarr.getX() +30, AIcarr.getY());
+                Hit hittttt = new Hit();
+                addObject(hittttt, AIcarr.getX() -60, AIcarr.getY());
+                Hit hitttttt = new Hit();
+                addObject(hitttttt, AIcarr.getX() +60, AIcarr.getY());
+            }
+            else
+            {
+                Hit hitt = new Hit();
+                addObject(hitt, AIcarr.getX(), AIcarr.getY());
+                Hit hittt = new Hit();
+                addObject(hittt, AIcarr.getX(), AIcarr.getY() -30);
+                Hit hitttt = new Hit();
+                addObject(hitttt, AIcarr.getX(), AIcarr.getY() +30);
+                Hit hittttt = new Hit();
+                addObject(hittttt, AIcarr.getX(), AIcarr.getY() -60);
+                Hit hitttttt = new Hit();
+                addObject(hitttttt, AIcarr.getX(), AIcarr.getY() +60);
+            }
+            
+            
+        }
+        
+        //int count = 0;
+        //int arrLength;
+        //for(int x = 50; x < 350; x += 30)
+        //{
+        //    for(int y = 25; y < 325; y += 30)
+        //    {
+        //        
+        //        arrLength = getObjectsAt(x, y, null).size();
+        //        Hit hitt = new Hit();
+        //        //System.out.println(arrLength);
+        //        /*
+        //        for(int i = 0; i < arrLength; i++)
+        //         {
+        //            System.out.println(getObjectsAt(x, y, null).get(i).toString());
+        //        }
+        //        */
+        //        if((arrLength != 1)&&(arrLength != 0))
+        //        {
+        //            addObject(hitt, x, y);
+        //            count++;
+        //        }
+        //        
+        //    }
+        //}
+        //System.out.println(count);
         
         
         
-        
+        /*
         for(int x = 50; x < 350; x += 30)
         {
             for(int y = 25; y < 325; y += 30)
@@ -183,7 +277,7 @@ public class GameWorld extends World
                 addObject(til2, x, y);
             }
         }
-        
+        */
         /*
         System.out.println("test");
         int length = getObjectsAt(50, 25, null).size();
@@ -222,8 +316,8 @@ public class GameWorld extends World
         Coord cord = new Coord("temp");
         addObject(cord, 500, 300);
         
-        Fire firee = new Fire();
-        addObject(firee, 425, 450);
+        Fire fireee = new Fire();
+        addObject(fireee, 375, 150);
     }
     
     public void human()
